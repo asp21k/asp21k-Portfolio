97bee048c067ff3d294de663042b655e63a07e12
asp21k <atharva.pardeshi0023@gmail.com>
2024-01-11 18:11:23 +0530

    vite config edited

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/asp21k-Portfolio/"
-  plugins: [react()],
+  base: "/asp21k-Portfolio/",
+  plugins: [react()]
  
 })
